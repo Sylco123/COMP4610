8dcbff5bb5322c6df420f57752c28e115f2d6d62
Sergio Barro <115595685+Sylco123@users.noreply.github.com>
2024-06-28 19:57:05 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Sergio Barro-Ojeda 01857597
 $(document).ready(function() {
     // Make tiles draggable
     $(".tile").draggable({
@@ -13,9 +14,9 @@ $(document).ready(function() {
             const droppedTile = ui.draggable.clone(); // Clone the draggable tile
             $(this).append(droppedTile);
 
-            // Update board space attributes (data-letter, data-value)
             const letter = droppedTile.attr("data-letter");
-            const value = parseInt(droppedTile.attr("data-value"));
+            const value = parseInt(droppedTile.attr("data-value")); // adding the point value to the assigned letter
+            // Update board space attributes (data-letter, data-value)
             $(this).attr("data-letter", letter);
             $(this).attr("data-value", value);
 
@@ -24,6 +25,17 @@ $(document).ready(function() {
         }
     });
 
+    // Make discard area droppable
+    $("#discard-area").droppable({
+        accept: ".tile",
+        drop: function(event, ui) {
+            const droppedTile = ui.draggable.clone();
+            $(this).append(droppedTile);
+        }
+    });
+
+    let cumulativeScore = 0;
+    
     // Function to update score based on board spaces
     function updateScore() {
         let totalScore = 0;
@@ -94,6 +106,8 @@ $(document).ready(function() {
             const newTile = $('<img>')
                 .attr('src', randomTileImage)
                 .addClass('tile')
+                .attr('data-letter', randomTileImage.slice(-5, -4)) // Adding tile value
+                .attr('data-value', getTileValue(randomTileImage.slice(-5, -4))) 
                 .draggable({
                     revert: "invalid",
                     containment: "body",
@@ -144,3 +158,14 @@ function isValidWord() {
     const dictionary = ["apple", "banana", "orange"]; // Example dictionary
     return dictionary.includes(word.toLowerCase());
 }
+
+    // Function to get tile value based on the letter
+    function getTileValue(letter) {
+        const tileValues = {
+            A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4,
+            I: 1, J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3,
+            Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8,
+            Y: 4, Z: 10, _: 0
+        };
+        return tileValues[letter.toUpperCase()] || 0;
+    }
